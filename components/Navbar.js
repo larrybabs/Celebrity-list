c656d9c569008d0b1b1a9fb35962670ed21d8802
Azeez Samad <seshp@ymail.com>
2022-04-15 13:48:19 +0100

    add link from next to navigate pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,22 @@
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="logo">
-      <nav>
-          <h1>LOGO</h1>
-          <a>Home</a>
-          <a>About</a>
-          <a>Celeb list</a>
-      </nav>
-    </div>
+    <nav>
+         <div className="logo">
+            <h1>LOGO</h1>
+         </div>
+          
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/celebs"> 
+            <a>Celeb list</a>
+          </Link>
+    </nav>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -20,6 +21,9 @@ export default function Home() {
         est. Esse non ullamco anim eiusmod. Officia adipisicing dolore minim
         nisi sunt aliquip do cillum pariatur fugiat.
       </p>
+      <Link href="/celebs">
+        <a>See Celebrities list</a>
+      </Link>
       <Footer />
     </div>
   )
