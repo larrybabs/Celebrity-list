025bdb079c3618af7be6dacbea88c6a22918b684
Azeez Samad <seshp@ymail.com>
2022-04-15 12:58:19 +0100

    add route to about page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from "react";
 
-export default function Home() {
+const about = () => {
   return (
     <div>
-      <Head>
-        <title>Celebrities</title>
-        <meta name="description" content="Celebrity list" />
-      </Head>
-      <h1>Trying something out</h1>
+      <h1>About</h1>
       <p>
         Consectetur deserunt ex laborum tempor enim nisi est. Esse non ullamco
         anim eiusmod. Officia adipisicing dolore minim nisi sunt aliquip do
@@ -18,5 +12,7 @@ export default function Home() {
         nisi sunt aliquip do cillum pariatur fugiat.
       </p>
     </div>
-  )
-}
+  );
+};
+
+export default about;
